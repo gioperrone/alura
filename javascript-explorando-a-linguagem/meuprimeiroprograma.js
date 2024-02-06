6c6f502eeaf9ffda4d6fbe5a63b0ae3957793315
Giovanni Perrone <giovannipublicidade@gmail.com>
2024-02-06 17:51:57 -0300

    criei o primeiro exercício de javascript

---
+++ javascript-explorando-a-linguagem/meuprimeiroprograma.js
@@ -0,0 +1,11 @@
+console.log("Meu Primeiro Programa: Trabalhando com Variáveis");
+
+const idade = 26;
+
+console.log(idade);
+console.log(idade+2);
+console.log(idade-2);
+console.log(idade/2);
+
+const idadeSomada = idade+2
+console.log(idadeSomada);
