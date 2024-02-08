fd317f4be5db4b934f3e8e865e43712200b44ec0
Giovanni Perrone <giovannipublicidade@gmail.com>
2024-02-08 15:10:16 -0300

    fiz exercício 7
    trabalhando com condicionais

---
+++ javascript-explorando-a-linguagem/7-trabalhandocomcondicionais.js
@@ -0,0 +1,30 @@
+console.log(`Trabalhando com Condicionais`);
+
+const listaDeDestinos = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`
+);
+
+const idadeComprador = 21;
+console.log(`Destinos Possíveis:`);
+console.log(listaDeDestinos);
+
+//para remover o item somente se o cliente tiver 18 anos ou mais (idadeComprador):
+if(idadeComprador >= 18) {
+    console.log("Comprador maior de idade. Passagem vendida.")
+    listaDeDestinos.splice(1,1);
+} else {
+    console.log("Comprador não é maior de idade. Por isso, não posso vender.")
+}
+
+console.log(listaDeDestinos);
+
+
+
+// OPERADORES LÓGICOS:
+// console.log(idadeComprador > 18);
+// console.log(idadeComprador < 18);
+// console.log(idadeComprador >= 18);
+// console.log(idadeComprador <= 18);
+// console.log(idadeComprador == 18);
