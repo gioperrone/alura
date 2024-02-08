d53dde8a2b92cc7fd41ea29b22e50befd30da720
Giovanni Perrone <giovannipublicidade@gmail.com>
2024-02-08 14:52:02 -0300

    finalizei exercício 6

--- javascript-explorando-a-linguagem/6-listas.js
+++ javascript-explorando-a-linguagem/6-listas.js
@@ -6,7 +6,11 @@ const listaDeDestinos = new Array(
     `Rio de Janeiro`
 ); //criando uma nova lista (Array)
 
-listaDeDestinos.push(`Curitiba`, `Manaus`); //adicionando um itens na lista
-
+listaDeDestinos.push(`Curitiba`, `Manaus`); //adicionando itens na lista
 console.log(`Destinos Possíveis:`);
 console.log(listaDeDestinos);
+
+listaDeDestinos.splice(1,1); //removendo itens da lista (posição do item, quantidade de itens) ATENÇÃO: A CONTAGEM COMEÇA EM 0 NÃO EM 1, por isso SP será removido
+console.log(listaDeDestinos);
+
+console.log(listaDeDestinos[1]); //[1] irá exibir somente o item 1 da lista (no caso, será RJ porque SP já foi removido no splice)
