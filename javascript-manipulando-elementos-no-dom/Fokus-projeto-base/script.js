86364d71a853d711081e39b3c37eebbbefcf5523
Giovanni Perrone <giovannipublicidade@gmail.com>
2024-02-23 18:00:18 -0300

    play e pause na música

--- javascript-manipulando-elementos-no-dom/Fokus-projeto-base/script.js
+++ javascript-manipulando-elementos-no-dom/Fokus-projeto-base/script.js
@@ -5,6 +5,18 @@ const longoBt = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
 const botoes = document.querySelectorAll('.app__card-button');
+const musicaFocoInput = document.querySelector('#alternar-musica');
+const musica = new Audio('/sons/luna-rise-part-one.mp3');
+musica.loop = true; //para a música não parar de tocar
+
+
+musicaFocoInput.addEventListener('change', () => {
+    if(musica.paused) {
+        musica.play();
+    } else {
+        musica.pause();
+    }
+});
 
 
 function alterarContexto(contexto) {
