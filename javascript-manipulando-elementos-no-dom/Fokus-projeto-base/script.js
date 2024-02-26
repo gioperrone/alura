d9aaf770d60a3f07b054b1158d7ab626a2d51ee9
Giovanni Perrone <giovannipublicidade@gmail.com>
2024-02-26 17:33:24 -0300

    adicionei sons nos botões

--- javascript-manipulando-elementos-no-dom/Fokus-projeto-base/script.js
+++ javascript-manipulando-elementos-no-dom/Fokus-projeto-base/script.js
@@ -5,9 +5,15 @@ const longoBt = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
 const botoes = document.querySelectorAll('.app__card-button');
+const startPauseBt = document.querySelector('#start-pause');
 const musicaFocoInput = document.querySelector('#alternar-musica');
 const musica = new Audio('/sons/luna-rise-part-one.mp3');
+const audioPlay = new Audio('/sons/play.wav');
+const audioPausa = new Audio('/sons/pause.mp3');
+const audioTempoFinalizado = new Audio('./sons/beep.mp3')
 musica.loop = true; //para a música não parar de tocar
+let tempoDecorridoEmSegundos = 5;
+let intervaloId = null;
 
 
 musicaFocoInput.addEventListener('change', () => {
@@ -69,8 +75,32 @@ longoBt.addEventListener('click', () => {
     longoBt.classList.add('active');
 });
 
+const contagemRegressiva = () => {
+    if(tempoDecorridoEmSegundos <= 0) {
+        audioTempoFinalizado.play();
+        zerar();
+        alert('Tempo finalizado!');
+        return;
+    }
+    tempoDecorridoEmSegundos -= 1;
+}
 
+startPauseBt.addEventListener('click', iniciarOuPausar);
 
+function iniciarOuPausar() {
+    if(intervaloId) {
+        audioPausa.play();
+        zerar();
+        return;
+    }
+    audioPlay.play();
+    intervaloId = setInterval(contagemRegressiva, 1000); //1000 milissegundos
+}
+
+function zerar() {
+    clearInterval(intervaloId);
+    intervaloId = null;
+}
 
 //CÓDIGO REPETITIVO:
 
