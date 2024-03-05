488d5b5f566f99dec05243b2e22d72bde60159c9
Giovanni Perrone <giovannipublicidade@gmail.com>
2024-03-05 12:58:15 -0300

    adicionando elementos na lista e correção de bugs

--- javascript-manipulacao-elementos-localstorage/Fokus-projeto-base/script-crud.js
+++ javascript-manipulacao-elementos-localstorage/Fokus-projeto-base/script-crud.js
@@ -1,8 +1,39 @@
 const btnAdicionarTarefa = document.querySelector('.app__button--add-task');
 const formAdicionarTarefa = document.querySelector('.app__form-add-task');
 const textarea = document.querySelector('.app__form-textarea');
+const ulTarefas = document.querySelector('.app__section-task-list');
 
-const ListaDeTarefas = [];
+const listaDeTarefas = JSON.parse(localStorage.getItem('listaDeTarefas')) || []
+
+function criarElementoTarefa(tarefa) {
+    const li = document.createElement('li');
+    li.classList.add('app__section-task-list-item');
+
+    const svg = document.createElement('svg');
+    svg.innerHTML = `
+        <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
+        <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
+        </svg>
+    `
+
+    const paragrafo = document.createElement('p');
+    paragrafo.textContent = tarefa.descricao;
+    paragrafo.classList.add('app__section-task-list-item-description');
+    
+    const botao = document.createElement('button');
+    botao.classList.add('app_button-edit');
+
+    const imagemBotao = document.createElement('img');
+    imagemBotao.setAttribute('src', '/imagens/edit.png');
+    botao.append(imagemBotao);
+
+    li.append(svg);
+    li.append(paragrafo);
+    li.append(botao);
+
+    return li;
+};
 
 
 btnAdicionarTarefa.addEventListener('click', () => {
@@ -14,6 +45,15 @@ formAdicionarTarefa.addEventListener('submit', (evento) => {
     const tarefa = {
         descricao: textarea.value
     };
-    ListaDeTarefas.push(tarefa);
-    localStorage.setItem('ListaDeTarefas', JSON.stringify(ListaDeTarefas));
+    listaDeTarefas.push(tarefa);
+    const elementoTarefa = criarElementoTarefa(tarefa);
+    ulTarefas.append(elementoTarefa);
+    localStorage.setItem('listaDeTarefas', JSON.stringify(listaDeTarefas));
+    textarea.value = '';
+    formAdicionarTarefa.classList.add('hidden');
+});
+
+listaDeTarefas.forEach(tarefa => {
+    const elementoTarefa = criarElementoTarefa(tarefa);
+    ulTarefas.append(elementoTarefa);
 });
--- javascript-manipulacao-elementos-localstorage/fokus-local-storage-main/script-crud.js
+++
@@ -1,5 +0,0 @@
-const btnAdicionarTarefa = document.querySelector('.app__button--add-task');
-
-btnAdicionarTarefa.addEventListener('click', () => {
-    
-});
