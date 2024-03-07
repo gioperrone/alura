117c13c0bed6ef1065af9399011b789c9ac1f146
Giovanni Perrone <giovannipublicidade@gmail.com>
2024-03-07 13:22:31 -0300

    selecionando e desselecionando uma tarefa

--- javascript-manipulacao-elementos-localstorage/Fokus-projeto-base/script-crud.js
+++ javascript-manipulacao-elementos-localstorage/Fokus-projeto-base/script-crud.js
@@ -3,8 +3,10 @@ const btnCancelar = document.querySelector('.app__form-footer__button--cancel');
 const formAdicionarTarefa = document.querySelector('.app__form-add-task');
 const textarea = document.querySelector('.app__form-textarea');
 const ulTarefas = document.querySelector('.app__section-task-list');
+const paragrafoDescricaoTarefa = document.querySelector('.app__section-active-task-description');
 
-const listaDeTarefas = JSON.parse(localStorage.getItem('listaDeTarefas')) || []
+const listaDeTarefas = JSON.parse(localStorage.getItem('listaDeTarefas')) || [];
+let tarefaSelecionada = null;
 
 function atualizarTarefas() {
     localStorage.setItem('listaDeTarefas', JSON.stringify(listaDeTarefas));
@@ -46,6 +48,20 @@ function criarElementoTarefa(tarefa) {
     li.append(svg);
     li.append(paragrafo);
     li.append(botao);
+
+    li.onclick = () => {
+        document.querySelectorAll('.app__section-task-list-item-active').forEach(elemento => {
+            elemento.classList.remove('app__section-task-list-item-active')
+        });
+        if(tarefaSelecionada == tarefa) {
+            paragrafoDescricaoTarefa.textContent = '';
+            tarefaSelecionada = null;
+            return;
+        };
+        tarefaSelecionada = tarefa;
+        paragrafoDescricaoTarefa.textContent = tarefa.descricao;
+        li.classList.add('app__section-task-list-item-active');
+    };
 
     return li;
 };
