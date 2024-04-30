a5d88526f6840107f6b3251f792a74b3b6358165
Giovanni Perrone <giovannipublicidade@gmail.com>
2024-04-30 13:37:22 -0300

    exibindo mensagens  no console

--- javascript-logica-programacao/app.js
+++ javascript-logica-programacao/app.js
@@ -1,3 +1,7 @@
 alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 5;
-prompt('Escolha um número entre 1 e 10');
+let chute = prompt('Escolha um número entre 1 e 10');
+
+if (chute == numeroSecreto) {
+    console.log('Isso aí! Você descobriu o número secreto (5).');
+};
