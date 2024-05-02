bc3931b3a8ebfbd3ebae704bc6778f84bff72ab9
Giovanni Perrone <giovannipublicidade@gmail.com>
2024-05-02 17:08:28 -0300

    concatenação e template strings

--- javascript-logica-programacao/app.js
+++ javascript-logica-programacao/app.js
@@ -1,7 +1,10 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 5;
+let numeroSecreto = 8;
+console.log('Número Secreto:' + numeroSecreto);
 let chute = prompt('Escolha um número entre 1 e 10');
 
 if (chute == numeroSecreto) {
-    console.log('Isso aí! Você descobriu o número secreto (5).');
+    alert(`Isso aí! Você descobriu o número secreto. (${numeroSecreto})`);
+} else {
+    alert('Você errou :(');
 };
