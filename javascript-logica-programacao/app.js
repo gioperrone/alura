b9aa0521fa394d2e4e0b6f36af1d2ee10b6b60b9
Giovanni Perrone <giovannipublicidade@gmail.com>
2024-05-03 15:18:28 -0300

    exibindo as tentativas

--- javascript-logica-programacao/app.js
+++ javascript-logica-programacao/app.js
@@ -2,18 +2,20 @@ alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 5;
 console.log('Número Secreto:' + numeroSecreto);
 let chute;
+let tentativas = 1;
 
 
 while (chute != numeroSecreto) {
     chute = prompt('Escolha um número entre 1 e 10');
 
     if (chute == numeroSecreto) {
-        alert(`Isso aí! Você descobriu o número secreto. (${numeroSecreto})`);
+        alert(`Isso aí! Você descobriu o número secreto (${numeroSecreto}) com ${tentativas} tentativa(s).`);
     } else {
         if (chute > numeroSecreto) {
             alert(`O número secreto é menor que ${chute}.`);
         } else {
             alert(`O número secreto é maior que ${chute}.`);
         };
+        tentativas++;
     };
 };
