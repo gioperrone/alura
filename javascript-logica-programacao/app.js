125a88250661171e4ee106995809a121155845d8
Giovanni Perrone <giovannipublicidade@gmail.com>
2024-05-03 15:12:17 -0300

    while

--- javascript-logica-programacao/app.js
+++ javascript-logica-programacao/app.js
@@ -1,10 +1,19 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 8;
+let numeroSecreto = 5;
 console.log('Número Secreto:' + numeroSecreto);
-let chute = prompt('Escolha um número entre 1 e 10');
+let chute;
 
-if (chute == numeroSecreto) {
-    alert(`Isso aí! Você descobriu o número secreto. (${numeroSecreto})`);
-} else {
-    alert('Você errou :(');
+
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 10');
+
+    if (chute == numeroSecreto) {
+        alert(`Isso aí! Você descobriu o número secreto. (${numeroSecreto})`);
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}.`);
+        } else {
+            alert(`O número secreto é maior que ${chute}.`);
+        };
+    };
 };
