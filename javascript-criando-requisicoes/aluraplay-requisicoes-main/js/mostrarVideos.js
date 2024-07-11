718c2df9d0457ff48b19e20eda9575b735fbee8c
Giovanni Perrone <giovannipublicidade@gmail.com>
2024-07-11 17:47:24 -0300

    tratamento de erros

--- javascript-criando-requisicoes/aluraplay-requisicoes-main/js/mostrarVideos.js
+++ javascript-criando-requisicoes/aluraplay-requisicoes-main/js/mostrarVideos.js
@@ -21,8 +21,12 @@ export default function constroiCard(titulo, descricao, url, imagem) {
 }
 
 async function listaVideos() {
-    const listaAPI = await conectaApi.listaVideos();
-    listaAPI.forEach(elemento => lista.appendChild(constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)));
+    try {
+        const listaAPI = await conectaApi.listaVideos();
+        listaAPI.forEach(elemento => lista.appendChild(constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)));
+    } catch {
+        lista.innerHTML = `<h2 class="mensagem__titulo">Não foi possível carregar a lista de vídeos.</h2>`;
+    }
 }
 
 listaVideos();
